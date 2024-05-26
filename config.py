eb0b7b142141dbb63ebf5b81b70591e96566816b
nakflix <123237114+nakflix@users.noreply.github.com>
2024-05-26 21:15:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6352502454:AAHyIL24fv8ASt6YJw0Wej8Nvvp03iZBZGY")
+API_ID = int(os.environ.get("API_ID", "14298205"))
+API_HASH = os.environ.get("API_HASH", "28df6d84da76d8606bf5f0e71ecfb62c")
+OWNER = int(os.environ.get("OWNER", "1458235021"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "The_Renamer_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "NAKFLIXTV ")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://alphakiti:alpha3720@cluster0.z5ggliv.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
+BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/568e33f4bb042042647ca.jpg")
 
 
 
